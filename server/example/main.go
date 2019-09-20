33082a4b638f2c915cb1265686d3aa522a577feb
qinhan <qinhan@xindong.com>
2019-09-20 18:23:09 +0800

    feat(server): add version in example server

--- server/example/main.go
+++ server/example/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+
+	"web-layout/utils/version"
+)
+
+var (
+	name   string
+	tag    string
+	commit string
+	branch string
 )
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +21,20 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var v bool
+	flag.BoolVar(&v, "version", false, "show version")
+	flag.Parse()
+
+	if v {
+		git := version.Info{
+			Name:   name,
+			Tag:    tag,
+			Commit: commit,
+			Branch: branch,
+		}
+		fmt.Println(git.Version())
+		return
+	}
 	http.HandleFunc("/ping", pingHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
