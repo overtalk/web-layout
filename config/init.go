b7b08379c5579e33f270a2ae26f29e4f881e5f74
秦翰 <qinhan@xindong.com>
2019-09-20 13:26:24 +0800

    refactor（config）：remove switch-case in NewConfig func

--- config/init.go
+++ config/init.go
@@ -6,23 +6,31 @@ import (
 )
 
 // Mode defines the server running mode
+// Mode defines the config source
 type Mode int
 
+// fetcher defines the uniform handler to get config from source
+type fetcher func(interface{}) error
+
 const (
+	// DEV mode get config from gitlab repo source
 	DEV = iota
+	// PROD mode get config from env variables
 	PROD
 )
+
+var fetchers map[Mode]fetcher
+
+func init() {
+	// collocate server running mode & config source
+	fetchers = map[Mode]fetcher{
+		DEV:  gitlab.Fetch,
+		PROD: env.Fetch,
+	}
+}
 
 // NewConfig is the resolver of config struct
 // input arg `config` should be pointer of config Struct
 func NewConfig(mode Mode, config interface{}) error {
-	var err error
-	switch mode {
-	case DEV:
-		err = gitlab.Fetch(config)
-	default:
-		err = env.Fetch(config)
-	}
-
-	return err
+	return fetchers[mode](config)
 }
