608e8791f70a10352baaffb68d64295eb56fcea3
qinhan <qinhan@xindong.com>
2019-09-20 19:41:17 +0800

    refactor(tools): refactor generate-mysql-model shell

--- tools/generate-mysql-model/gen-model.sh
+++ tools/generate-mysql-model/gen-model.sh
@@ -1,26 +1,35 @@
 #!/usr/bin/env bash
 
-# import related config
-source .config
-convert_tool=db2struct
-
-
 # check if db2struct exist
-if !(hash ${convert_tool} 2>/dev/null); then
-  echo "convert tool ${convert_tool} is absent, install..."
-  go get github.com/Shelnutt2/db2struct/cmd/db2struct
-fi
+check()
+{
+
+  if !(hash ${convert_tool} 2>/dev/null); then
+    echo "convert tool ${convert_tool} is absent, install..."
+    go get github.com/Shelnutt2/db2struct/cmd/db2struct
+  fi
+}
 
 
 # generate golang model
-for table in ${TABLES[@]};
-do
-  echo "del old model/${table}.go" && rm ../../model/${table}.go
-  struct_name="$(echo ${table:0:1} | tr '[a-z]' '[A-Z]')${table:1}"
-  db2struct -H ${HOST} -u ${USER} -p ${PASSWORD}  \
-    --package ${GO_PACKAGE_NAME} -d ${DB}  \
-    --table ${table} --struct ${struct_name}  \
-    --target=../../model/${table}.go \
-    --json -v
-  echo "finish model/${table}.go\n"
-done
+gen()
+{
+  for table in ${TABLES[@]};
+  do
+    echo "del old model/${table}.go" && rm ../../model/${table}.go
+    struct_name="$(echo ${table:0:1} | tr '[a-z]' '[A-Z]')${table:1}"
+    db2struct -H ${HOST} -u ${USER} -p ${PASSWORD}  \
+      --package ${GO_PACKAGE_NAME} -d ${DB}  \
+      --table ${table} --struct ${struct_name}  \
+      --target=../../model/${table}.go \
+      --json -v
+    echo "finish model/${table}.go\n"
+  done
+}
+
+
+# import related config
+source .config
+convert_tool=db2struct
+check
+gen
